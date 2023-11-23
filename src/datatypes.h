a9fc87c6cd71aa4b393de433b60a4b4d180fb1ef
Martin Lejko <martinlejko@Matkos-MacBook-Air.local>
2023-11-23 21:47:49 +0100

    Separated custom datatypes into individual header

---
+++ src/datatypes.h
@@ -0,0 +1,18 @@
+//
+// Created by Martin Lejko on 23/11/2023.
+//
+
+#ifndef SRC_DATATYPES_H
+#define SRC_DATATYPES_H
+struct Point2D {
+    int x;
+    int y;
+};
+
+struct Point3D {
+    float x;
+    float y;
+    float z;
+};
+
+#endif //SRC_DATATYPES_H
